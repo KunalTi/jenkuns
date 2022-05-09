b96954f1548c053cb5ec344f0e8dad153671645c
KunalTi <92357008+KunalTi@users.noreply.github.com>
2022-05-09 09:34:37 +0530

    worker node

--- jrnkins.groovy
+++ jrnkins.groovy
@@ -6,7 +6,6 @@ pipeline {
                  stage('Build') {
                  steps {
                      echo ' Starting to build the App.'
-                     git credentialsId: 'git_jenkins', url: 'git@github.com:KunalTi/jenkuns.git'
                      sh "ls"
                  sh 'sleep 10'
                  }
