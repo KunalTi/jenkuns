f0386f1b5ea5f168c48b0dfeaf8e6589e13f302d
Kunal <ktiple64@gmail.com>
2022-05-22 17:37:16 +0530

    changes

--- PHP.groovy
+++ PHP.groovy
@@ -12,7 +12,7 @@ pipeline{
                 sudo apt-get install php libapache2-mod-php -y
                 sudo apt install php-dev libmcrypt-dev php-pear -y
                 sudo pecl channel-update pecl.php.net
-                sudo pecl install mcrypt-1.0.4
+                sudo pecl install mcrypt-1.0.4 --skipInstalled
                 sudo service apache2 restart
                 sudo rm -rf /var/www/html/index.html
                 sudo cp /var/lib/jenkins/workspace/'PHP Pipeline'/test.php /var/www/html/test.php
