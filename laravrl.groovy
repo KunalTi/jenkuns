6dc63527bc042e4a82ad0b61009cc997ee1c0a3e
Kunal <ktiple64@gmail.com>
2022-06-15 12:52:31 +0530

    laravel

---
+++ laravrl.groovy
@@ -0,0 +1,30 @@
+
+pipeline {
+    agent any
+    stages {
+        stage('Build') {
+            steps {
+                echo 'Building..'
+                sh '''
+                sudo apt-get update -y
+                sudo apt-add-repository ppa:ondrej/php
+                sudo apt-get install php7.2-cli php7.2-fpm php7.2-fpm php7.2-xml php7.2-gd php7.2-mysql php7.2-mbstring php7.2-curl php7.1-mcrypt
+                php -v
+                sudo apt-ger install mysql-server -y
+                sudo apt install nginx -y
+                sudo apt install zip unzip -y
+                '''
+            }
+        }
+        stage('Test') {
+            steps {
+                echo 'Testing..'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'Deploying....'
+            }
+        }
+    }
+}
