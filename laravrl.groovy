733007449091a9c8a8f8567505bf93f45dacc72c
Kunal <ktiple64@gmail.com>
2022-06-15 17:53:15 +0530

    la

--- laravrl.groovy
+++ laravrl.groovy
@@ -10,9 +10,17 @@ pipeline {
                 sudo apt-add-repository ppa:ondrej/php
                 sudo apt install php7.2-cli php7.2-fpm php7.2-fpm php7.2-xml php7.2-gd php7.2-mysql php7.2-mbstring php7.2-curl php7.1-mcrypt -y
                 php -v
-                sudo apt install mysql -y
                 sudo apt install nginx -y
                 sudo apt install zip unzip -y
+                git clone https://github.com/KunalTi/aws-laravel.git
+                cd aws-laravel
+                curl -aS https://getcomposer.org/installer | sudo php -- --install-dir /usr/local/bin/ --filename=composer
+                composer install --no-dev
+                cp .env.example .env
+                php artisan key:generate
+                cd ~
+                cd /etc/nginx
+                
                 '''
             }
         }
