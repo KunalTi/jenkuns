64ef00f40a1944142f1386b36a4b44034557146b
Kunal <ktiple64@gmail.com>
2022-06-15 13:46:13 +0530

    la

--- laravrl.groovy
+++ laravrl.groovy
@@ -10,7 +10,7 @@ pipeline {
                 sudo apt-add-repository ppa:ondrej/php
                 sudo apt-get install php7.2-cli php7.2-fpm php7.2-fpm php7.2-xml php7.2-gd php7.2-mysql php7.2-mbstring php7.2-curl php7.1-mcrypt -y
                 php -v
-                sudo apt-get install mysql-server -y
+                sudo apt-get install mysql -y
                 sudo apt install nginx -y
                 sudo apt install zip unzip -y
                 '''
